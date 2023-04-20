9f04b9bc7d353d145ada28b4ada2106cdc0cab89
Relasey <worlase.adorme@gmail.com>
2023-04-20 16:49:03 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char go));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int so));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int to));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long long int do));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long long int doe));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float no));
 	return (0);
 }
