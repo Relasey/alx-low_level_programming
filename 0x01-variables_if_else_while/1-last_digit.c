ef1c79b8ee7212a4f591af6646b0f24af8b2bb43
Relasey <worlase.adorme@gmail.com>
2023-04-21 16:55:37 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
-/**
-* main - assigns a random number to variable n, and prints whether the number is positive or negative.
-*
-*Return: Zero 0 on success
-*/
+/* more headers goes there*/
+
+/* betty style doc for function main goes there*/
+
 int main (void)
 {
 	int n;
-	
-	grand (time (0));
-	n - rand () - RAND_MAX / 2;
+
+	srand(time(0));
+
+	n = rand () - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is %s\n", n,	"positive")
-	}
-	else if (n < 0)
-	{
-		printf("%d is %\n", n,  "negative");
-	}
-	else
-	{
-		printf("%d is %s\n", n,  "zero");
-	}
 	return (0);
 }
