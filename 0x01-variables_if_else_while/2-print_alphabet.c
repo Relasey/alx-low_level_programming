04fce44073502b63507cb690e9943b87c4d3f0d9
Relasey <worlase.adorme@gmail.com>
2023-04-21 19:37:48 -0700

    y

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,21 @@
 int main(void)
 {
 	char c;
+	
+	char d;
 
 	c = 'a';
+	d = 'A';
 	while
-		(c <= 'z') {
+		(c <= 'z'){
 			putchar(c);
 			c++;
-
 		}
-	putchar("\n");
+	while
+		(d <= 'Z'){
+       	        	putchar(d);
+			d++;
+		}
+	putchar('\n');
 	return (0);
+}
