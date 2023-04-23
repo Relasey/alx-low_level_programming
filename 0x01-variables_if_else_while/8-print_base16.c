c768487a62540af768ad08df48d70ac030693ecd
Relasey <worlase.adorme@gmail.com>
2023-04-23 10:51:42 -0700

    baseform

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 59; i++)
-{
+	for (i = 48; i < 58; i++)
+	{
 		putchar(i);
-}
+	}
 	for (i = 97; i < 103; i++)
-{
+	{
 		putchar(i);
-}
+	}
 	putchar('\n');
 	return (0);
 }
