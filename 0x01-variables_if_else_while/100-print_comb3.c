3d4d758e1e745b8fab7fa319cd445ee41e9d3dc6
Relasey <worlase.adorme@gmail.com>
2023-04-24 16:56:47 -0700

    variables update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
-
 /**
- * main - prints all possible different combinations of three digits
+ * main - prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n, m, l;
+	int n, m;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n < 9; n++)
 	{
-		for (m = 49; m < 58; m++)
+		for (m = n + 1; m < 10; m++)
 		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+			putchar((n % 10) + '0');
+			putchar((m % 10) + '0');
+			if (n == 8 && m == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
