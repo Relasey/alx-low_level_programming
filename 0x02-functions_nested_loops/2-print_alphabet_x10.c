bb5b5c9b6f02c4e44469225b94c798ab1760a29f
Relasey <worlase.adorme@gmail.com>
2023-04-25 16:34:09 -0700

    alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ *followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int alphabet = 97;
+
+	while (alphabet < 123)
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+}
