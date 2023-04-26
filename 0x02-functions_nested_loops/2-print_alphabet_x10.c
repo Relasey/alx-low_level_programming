694f5844828afd5162ae30f73c4d4aed23976e51
Relasey <worlase.adorme@gmail.com>
2023-04-26 11:07:26 -0700

    variable

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- *followed by a new line
+ *
  */
 void print_alphabet_x10(void)
 {
-	int alphabet = 97;
+	int alphabet = 0;
 
-	while (alphabet < 123)
+	while (alphabet < 10)
 	{
 		_putchar(alphabet);
 		alphabet++;
