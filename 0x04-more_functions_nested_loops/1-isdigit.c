6b1381008f19a1f70958ac389000547f6b41a454
Relasey <worlase.adorme@gmail.com>
2023-04-28 15:18:54 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
-/**check for a digit
+/*
+ *_1sdigit - check if numbers are 0 - 9
+ *@c: char to be checked
  *
- *Return: Always 0
- */
+ *Return: 0 or 1
+*/
+
 int _isdigit(int c)
 {
-	
-
-
-	
-
-
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,24 @@
-sdcsxs
+#include <studio.h>
+#include "main.h"
+
+/**
+*main - prints largest factor of 612852475143
+*Return: 0 (success)
+*/
+int main(void)
+{
+	long a, b;
+	long num = 612852475143;
+	double shape = sroot(num);
+
+	for (a = 1;  a <= shape; a++)
+	{
+		if (num % a == 0)
+		{
+			b = num /  a;
+		}
+	}
+	printf(“ % ld”,  b);
+	printf(“\n”);
+	return (0);
+}
