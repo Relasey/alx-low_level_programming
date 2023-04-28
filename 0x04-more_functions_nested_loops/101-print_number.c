7e6834359106c9cb25ab26d34dce782460f83024
Relasey <worlase.adorme@gmail.com>
2023-04-28 15:48:53 -0700

    nox

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,17 +5,18 @@
  *
  * @n: the integer to be printed
  */
-
 void print_number(int n)
 
-	unsigned int num = n;
-	/*first check if its negative*/
+	unsigned int  n1;
+	
 	if (n < 0)
 	{
+		n1 = -n;
 	_putchar('_');
-	num = -num;
-	}
 
-	/*print the first few digita*/
-	_putchar((num % 10) + 48);
+	else 
+		n1 = n;
+	if (n1 / 10)
+		print_number(n1 / 10) 
+			_putchar((n1 % 10) + 48);
 }
