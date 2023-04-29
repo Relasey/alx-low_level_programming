c0e38ec2fc6cd421a9b144ff447d5db1e3f380ea
Relasey <worlase.adorme@gmail.com>
2023-04-28 22:32:07 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,83 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-*main - prints largest factor of 612852475143
-*Return: 0 (success)
-*/
+ * main - prints the biggest prime factor of a number.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	long a, b;
-	long num = 612852475143;
-	double shape = sroot (num);
+	long int number;
 
-	for (a = 1;  a <= shape; a++)
+	number = 612852475143;
+
+	if (isPrime(number) == 1)
 	{
-		if (num % a == 0)
+		printf("%ld\n", number);
+	}
+	else
+	{
+		printf("%ld\n", biggestFactor(number));
+	}
+	return (0);
+}
+/**
+ * isPrime - analise if a number is prime or not
+ * @n: number to check
+ * Return: true if it is prime false if not
+ */
+int isPrime(long int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		for (i = 2; i < n; i++)
 		{
-			b = num /  a;
+			if (n % i == 0)
+			{
+				return (0);
+			}
+		}
+		return (1);
+	}
+}
+/**
+ * biggestFactor - returns the biggest prime factor of a number
+ * @a: number to check
+ * Return: biggest factor
+ */
+long int biggestFactor(long int a)
+{
+	long int i, factor;
+
+	factor = a;
+	for (i = 2; i <= factor; i++)
+	{
+		if (isPrime(factor) == 1)
+		{
+			break;
+		}
+		else
+		{
+			if ((factor % i == 0) && (isPrime(i) == 1))
+			{
+				factor = factor / i;
+				continue;
+			}
+			else
+			{
+				factor = factor;
+			}
 		}
 	}
-	printf(" % ld",  b);
-	printf("\n");
-	return (0);
+	return (factor);
 }
