0a1086b166ea428891a1c6441b62b9be613f8873
Relasey <worlase.adorme@gmail.com>
2023-05-02 20:57:00 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+*print_array - prints n element of an array of integers
+*@str: the input array
+*@n: the largest of the array
+*/
+void print _array(int *a, int n)
+{
+	int index_of_array;
+	{
+		printf("%d", a[index_of_array]);
+		if (index_of_array ! = [n - 1])
+		{
+			printf(",");
+		}
+				putchar('\n')
+	}
+}
