6fe3ffe21b1067fd46eb9fef2312fd323f23356b
Relasey <worlase.adorme@gmail.com>
2023-05-02 21:22:19 -0700

    slop

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+*_stropy - function that copies the string
+*
+*@dest: pointer to destination char
+*@src: pointer to source char
+*Return:char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int a = -1;
+	{
+		do 
+			z++;
+		dest [z] =src );
+		while (src[z] != '\0');
+		return (dest);
+	}
