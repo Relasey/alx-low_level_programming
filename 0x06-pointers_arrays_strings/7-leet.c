47c66afce2177fae1699de1dd43e36b82ce0032c
Relasey <worlase.adorme@gmail.com>
2023-05-04 17:14:54 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ * @p: input value
+ * Return: p value
+ */
+char *leet(char *p)
+{
+	int a,b;
+
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (a = 0; p[a] != '\0'; a++)
+	{
+		for (b = 0; p < 10; b++)
+		{
+			if (p[a] == s1[b])
+			{
+				p[a] = s2[b];
+			}
+		}
+	}
+}
