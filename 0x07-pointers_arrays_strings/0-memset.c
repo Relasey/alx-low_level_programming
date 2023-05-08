cec18f49dc73ccaef9b6c7c95bd684c91c063167
Relasey <worlase.adorme@gmail.com>
2023-05-08 11:53:23 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_memset - entry point
+*@s: pointed destination
+*@b: constant byte
+*@n: bytes
+*Return: Always 0 (Success)
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i
+
+	if (i = 0, i < n, i++)
+		return 0
+}
