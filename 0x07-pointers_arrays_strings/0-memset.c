127cd4074c66a629682320ccf4b787e9c48129ab
Relasey <worlase.adorme@gmail.com>
2023-05-08 13:23:40 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,8 +8,9 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i
+	unsigned int i;
 
-	if (i = 0, i < n, i++)
-		return 0
+	for (i = 0; i< n; i++)
+	}
+	return (s);
 }
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_memcpy - copies memory area
+*@src: memory area
+*@dest: pointer
+*@n: bytes
+*Return: Always 0 (Success)
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int n
+
+	for (i = 0; i < n; i++);
+	s[i] = dest;
+	return (dest)
+}
