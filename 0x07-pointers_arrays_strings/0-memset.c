a2d57375ce251a2c2d5bff63064b1925c3c07a41
Relasey <worlase.adorme@gmail.com>
2023-05-09 13:00:27 -0700

    mem

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - fills memory with a constant byte.
+ * *_memset - fills memory with a constant byte.
  * @s: source string
  * @b: the contant byte for filling
  * @n: lenght of buffer
