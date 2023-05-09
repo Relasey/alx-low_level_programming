d01f81b5501444246746aafa341d66835e5e9c21
Relasey <worlase.adorme@gmail.com>
2023-05-09 13:22:20 -0700

    str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *_strchr - prints from the first occurrence of a char.
- *@s: source string
- *@c: tested char
+ * *_strchr - prints from the first occurrence of a char.
+ * @s: source string
+ * @c: tested char
  *
- *Return: new string.
+ * Return: new string.
  */
 
 char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; s[a] >= ‘\0’; a++)
+	for (a = 0; s[a] >= '\0'; a++)
 	{
 		if (s[a] == c)
 			return (s + a);
