efed38f76f144eb6b61016a2a6f77864b19cca6b
Relasey <worlase.adorme@gmail.com>
2023-05-18 14:07:47 -0700

    call

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - prints calloc
+* @nmemb: array of memory
+* @size: element
+* Return: Pointer.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *q;
+	unsigned int i, b;
+
+	if (nmemb <= 0 || size <= 0)
+		return NULL;
+
+	b = nmemb * size;
+	q = malloc(b);
+
+	if (q == NULL)
+	       	return NULL;
+
+	for (i = 0; i < b; i++)
+	{
+		q[i] = 0;
+	}
+}
