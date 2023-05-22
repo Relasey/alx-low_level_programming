d4d67eee308655f9586e1da3a59d0a87d87209f8
Relasey <worlase.adorme@gmail.com>
2023-05-22 11:24:36 -0700

    dogh

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+  * struct dog - The dog attributes
+  * @name: dog name.
+  * @age: dog age.
+  * @owner: dog owner.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
