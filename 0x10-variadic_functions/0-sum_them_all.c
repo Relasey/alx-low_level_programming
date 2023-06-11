365a657e8b6fba03b2ec21c30e151c05bc98cb0f
Relasey <worlase.adorme@gmail.com>
2023-06-11 03:53:21 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,10 @@
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - sums all arguments
+ * @n: number of arguments
+ * Return: sum of all arguments
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	register unsigned int a;
