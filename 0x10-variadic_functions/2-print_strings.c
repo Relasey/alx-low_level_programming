d4e7ab42833210250df4a89da442ec726f748fa7
Relasey <worlase.adorme@gmail.com>
2023-06-11 03:56:33 -0700

    print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,11 @@
 #include "variadic_functions.h"
 
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string separator
+ * @n: number of arguments
+ */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	register unsigned int i;
