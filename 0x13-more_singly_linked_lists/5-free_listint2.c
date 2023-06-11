373ce4dfcfae8ec374092a4745cfe5f566bf9d32
Relasey <worlase.adorme@gmail.com>
2023-06-11 05:18:16 -0700

    free listinc

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: double pointer
+ *
+ * Return: 0
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == 0)
+		return;
+	do {
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	} while (*head != 0);
+}
