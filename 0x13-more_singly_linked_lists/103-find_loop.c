da87558b77fd7c0aa55f43b9cf214a208a13199d
Relasey <worlase.adorme@gmail.com>
2023-06-11 06:31:16 -0700

    last

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - func that finds the loop in a linked list.
+ * @head:  pointer
+ * Return: node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *obj1, *obj2;
+
+	obj1 = obj2 = head;
+	while (obj1 && obj2 && obj2->next)
+	{
+		obj1 = obj1->next;
+		obj2 = obj2->next->next;
+		if (obj2 == obj1)
+		{
+			obj1 = head;
+			break;
+		}
+	}
+	if (!obj2 || !obj1 || !obj2->next)
+		return (NULL);
+	while (obj1 != obj2)
+	{
+		obj1 = obj1->next;
+		obj2 = obj2->next;
+	}
+	return (obj2);
+}
