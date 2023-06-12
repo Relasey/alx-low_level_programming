3c204b75c93d5699caf5c843ebae97d91cf9484b
Relasey <worlase.adorme@gmail.com>
2023-06-12 13:54:37 -0700

    hello world

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,21 +11,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w,len = 0;
+	int fd, wr, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	wr = write(fd, text_content, length);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || wr == -1)
 		return (-1);
 
 	close(fd);
