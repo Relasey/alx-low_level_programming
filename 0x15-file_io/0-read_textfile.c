e9234810e3dfe195e087266499922f78361636ad
Relasey <worlase.adorme@gmail.com>
2023-06-16 22:11:46 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,19 +10,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *buffer;
+	int fd;
+	ssize_t _r, _w;
 
+	if (!filename)
+	{
+		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-
-	free(buf);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	_r = read(fd, buffer, letters);
+	if (_r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	_w = write(STDOUT_FILENO, buffer, _r);
+	if (_w == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 	close(fd);
-	return (w);
+	return (_w);
 }
